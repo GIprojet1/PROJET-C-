84744c710fc8af63cac4c48ec76dd3d03a68d19f
GIprojet1 <ibtissamessafaoui10@gmail.com>
2024-05-09 22:03:45 +0200

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,12 @@
+string product::getName(){
+	return name;
+}
+int product::getQantity(){
+	return quantity;
+}
+float product::getPrice(){
+	return price;
+}
+void product::setQuantity(int x)
+    quantity=x	
+}
